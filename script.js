a5b98c519ba6b5e7145369a89978550eec5bdf2f
Ashish Chaurasiya <ashishkrdc@gmail.com>
2023-04-04 09:34:18 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const scrollToTopBtn = document.querySelector("#scrollToTopBtn");
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.addEventListener("click", function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
